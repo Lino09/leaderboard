de61f13da87c9905c0f3fd5a7db6de0f5254cae9
Lino09 <onosmx@gmail.com>
2021-11-09 16:03:16 -0600

    Add reset file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-import _ from 'lodash';
+import './reset.css';
 import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
